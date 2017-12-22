3e6abcf5038c7e42f153122914e30af647b68089
shivprogrammer <shiv.unadkat@gmail.com>
2017-12-22 14:04:22 -0800

    the game is acknolwedging button presses [space, "a", "d"]

---
+++ Spaceship_Game/Assets/Rocket.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput() {
+        if (Input.GetKey(KeyCode.Space)) {
+            print("The space button has been pressed");
+        }
+        else if (Input.GetKey("a")) {
+            print("the player is rotating left");
+        }
+        else if (Input.GetKey("d")) {
+            print("the player is rotating right");
+        }
+    }
+}
