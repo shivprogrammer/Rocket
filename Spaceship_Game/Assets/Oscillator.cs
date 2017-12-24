441aef600433ee32bd37b8fbe90de8a66ae7eb80
shivprogrammer <shiv.unadkat@gmail.com>
2017-12-24 10:37:41 -0800

    OBSTACLES ARE OSCILLATING SON

--- Spaceship_Game/Assets/Oscillator.cs
+++ Spaceship_Game/Assets/Oscillator.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour {
 
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 movementVector = new Vector3(0f, -15f, 0f);
+    [SerializeField] float period = 4f;
 
     // TODO remove from inspector later
     [Range(0, 1)] [SerializeField] float movementFactor; // 0 for not moved, 1 for fully moved
@@ -19,6 +20,13 @@ public class Oscillator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        float cycles = Time.time / period;
+
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave / 2f + 0.5f;
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
 	}
