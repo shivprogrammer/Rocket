be07f574ed3cdd07e0e4149704da56d4bb669130
shivprogrammer <shiv.unadkat@gmail.com>
2017-12-23 23:25:58 -0800

    adding obstacle movement

---
+++ Spaceship_Game/Assets/Oscillator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour {
+
+    [SerializeField] Vector3 movementVector;
+
+    // TODO remove from inspector later
+    [Range(0, 1)] [SerializeField] float movementFactor; // 0 for not moved, 1 for fully movedd
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
