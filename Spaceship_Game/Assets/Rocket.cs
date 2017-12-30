f62fb6717490fdbbb77e9bb1d2461e0671b285b2
shivprogrammer <shiv.unadkat@gmail.com>
2017-12-29 20:57:43 -0800

    added the ability to load through all levels

--- Spaceship_Game/Assets/Rocket.cs
+++ Spaceship_Game/Assets/Rocket.cs
@@ -84,7 +84,12 @@ public class Rocket : MonoBehaviour {
     }
 
     private void LoadNextScene() {
-		SceneManager.LoadScene(1);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings) {
+            nextSceneIndex = 0;
+        }
+		SceneManager.LoadScene(nextSceneIndex);
     }
 
     private void LoadOnDeath() {
